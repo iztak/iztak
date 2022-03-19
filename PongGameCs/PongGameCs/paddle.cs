0ac3e0c5287a00289e50a5afdea97b49ebe0a45a
iztak <101916986+iztak@users.noreply.github.com>
2022-03-19 14:26:11 +0900

    Add files via upload

---
+++ PongGameCs/PongGameCs/paddle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PongGameCs
+{
+ class paddle
+ {
+        int paddlex;
+        int paddley;
+        int size = 200;
+
+    paddlex = paddle_x;
+
+    
+        
+ }
+}
